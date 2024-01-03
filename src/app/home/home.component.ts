109d95bd2a797b8e92794e763703867460528e5f
Sondos Sami <sondos.sami.abuzeid@gmail.com>
2024-01-03 12:11:03 +0200

    chore: add comments to ngOnInit and remove unused constructor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,38 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "../api.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  users = [];
+  comments = [];
+  photos = [];
+  posts = [];
 
-  users=[];
-  comments=[];
-  photos=[];
-  posts=[];
-
-  constructor( private _ApiService:ApiService) {
-    this._ApiService.getComments().subscribe(data => {
+  constructor(private _ApiService: ApiService) {
+    this._ApiService.getComments().subscribe((data) => {
       this.comments = data;
-    })
+    });
 
-    this._ApiService.getPhotos().subscribe(data => {
+    this._ApiService.getPhotos().subscribe((data) => {
       this.photos = data;
       // console.log(this.photos);
-    })
+    });
 
-    this._ApiService.getPosts().subscribe(data => {
+    this._ApiService.getPosts().subscribe((data) => {
       this.posts = data;
-    })
+    });
 
-    this._ApiService.getUsers().subscribe(data => {
+    this._ApiService.getUsers().subscribe((data) => {
       this.users = data;
-    })
+    });
   }
 
   ngOnInit() {
+    // Remove the empty ngOnInit if not needed
   }
-
 }
